07e2cf7a9bfa463b5d8b9f7d4ffebcf4a938c1d0
Diogo Felismino da Silva <diogo.silva@notacontrol.matriz>
2022-05-30 17:07:52 -0400

    Incio do Projeto sobre CRUD

---
+++ WebCrudLogAuditoria/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCrudLogAuditoria.Models;
+
+namespace WebCrudLogAuditoria.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<LogAuditoria> LogAuditoria { get; set; }
+    }
+}
---
+++ WebCrudLogAuditoria/Models/LogAuditoria.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCrudLogAuditoria.Models
+{
+
+    [Table("LogAuditoria")]
+    public class LogAuditoria
+    {
+
+
+        [Column("Id")]
+        [Display(Name = "Código")]
+
+        public int Id { get; set; }
+
+        [Column("DetalhesAuditoria")]
+        [Display(Name = "Detalhes Auditoria")]
+
+        public string DetalhesAuditoria { get; set; }
+
+        [Column("EmailUsuario")]
+        [Display(Name = "Email Usuario")]
+
+        public string EmailUsuario { get; set; } 
+
+    }
+}
